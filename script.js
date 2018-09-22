fae120232cde9d94ae31b3476b1b8be879dc2d74
vicduraes <duraes.vic@gmail.com>
2018-09-21 22:26:58 -0300

    Corrigir JS

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ botao.addEventListener("click", function (event) {
   numero = parseInt(document.querySelector("#numero").value);
   resultado = document.querySelector("#resultado");
   for (var i = 0; i <= 10; i++) {
-    console.log(numero * i);
-    document.getElementById ("resultado").innerHTML = + '<p>' + numero * i + '</p>';
+    var p = document.createElement('p');
+    p.innerHTML = i + " x " +numero + " = "+ numero * i;
+    resultado.appendChild(p);
+    // document.getElementById ("resultado").innerHTML =+  i + '<br>';
     // document.querySelector("resultado").textContent = numero * i;
   }
   // if (numero != number.value){
